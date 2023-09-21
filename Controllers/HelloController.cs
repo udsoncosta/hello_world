10353b984d3b406e3643fca422f0ca99bde90d99
udsoncosta <udsoncostasantana@gmail.com>
2023-09-21 17:02:10 -0300

    HelloApi

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace helloworld.Controllers
+{
+    [Route("~/")]
+    [ApiController]
+    public class HelloController
+    {
+        [HttpGet]
+        public string Hello()
+        {
+            return "Hello Turma .NET 03!";
+        }
+        [HttpGet("/meunome")]
+        public string MeuNome()
+        {
+            return "Udson";
+        }
+    }
+}

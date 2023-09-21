2acc05a7f369e4761090ccdf194b94903ff00e8e
udsoncosta <udsoncostasantana@gmail.com>
2023-09-21 17:16:16 -0300

    aspnethelloworld

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -16,5 +16,15 @@ namespace helloworld.Controllers
         {
             return "Udson";
         }
+        [HttpGet("/bsms")]
+        public string BSMS()
+        {
+            return "Proatividade\nComunicação\nPersistência\nOrientação ao Detalhe\nMentalidade de Crescimento\nResponsabilidade Pessoal\nOrientação ao Futuro";
+        }
+        [HttpGet("/objetivos")]
+        public string Objetivos()
+        {
+            return "Criar Blog Pessoal\nAprender Asp Net\nEstudar Banco de Dados";
+        }
     }
 }
